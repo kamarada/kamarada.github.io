1e570801d4fd3b92e64dcd96be416c6cb343862e
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2017-08-11 16:02:34 -0300

    Publishing website (see commit 931705d7b2b93450c185ecda33a88f34f787a08f from kamarada-website)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,29 @@
-var $search_input = null;
-var do_search = false;
-
-function hide_search_input() {
-    $('#search-input').animate({
-        'width': 0,
-        'opacity': 0
-    }, 200);
-}
-
 function random_number(first, last) {
     return Math.floor((Math.random() * (last - first + 1))) + first;
-}
-
-function submit_search_form() {
-    do_search = true;
-    $('#search-form').submit();
 }
 
 $(document).ready(function(){
     $.material.init();
 
-    /* Search form */
+    /* Search (based on: https://bootsnipp.com/snippets/a6aV0) */
 
-    /*$search_input = $('#search-input');
-
-    $('#search-button').click(function(event) {
+    $('#search-button, #reset-search-button').on('click', function(event) {
         event.preventDefault();
-        if ($search_input.width() > 0) {
-            if ($search_input.val()) {
-                submit_search_form();
-            } else {
-                hide_search_input();
-            }
-        } else {
-            $search_input.animate({
-                'width': '182px',
-                'opacity': 1
-            }, 300, function() {
-                $search_input.focus();
-            });
+        $('#search-input').val('');
+        $('.navbar-with-search .search-bar').toggleClass('open');
+        $('#search-button').closest('li').toggleClass('active');
+
+        if ($('.navbar-with-search .search-bar').hasClass('open')) {
+            /* I think .focus doesn't like css animations, set timeout to make sure input gets focus */
+            setTimeout(function() {
+                $('#search-input').focus();
+            }, 100);
         }
     });
 
-    $search_input.keypress(function(event) {
-        if (event.which == 10 || event.which == 13) {
-            submit_search_form();
+    $(document).on('keyup', function(event) {
+        if (event.which == 27 && $('.navbar-with-search .search-bar').hasClass('open')) {
+            $('#search-button').trigger('click');
         }
-    }).blur(function() {
-        if (!$search_input.val() && !do_search) {
-            hide_search_input();
-        }
-    });*/
-
-    $('a[href="#toggle-search"], .btn[type="reset"]').on('click', function(event) {
-		event.preventDefault();
-		$('.navbar-bootsnipp .bootsnipp-search .input-group > input').val('');
-		$('.navbar-bootsnipp .bootsnipp-search').toggleClass('open');
-		$('a[href="#toggle-search"]').closest('li').toggleClass('active');
-
-		if ($('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
-			/* I think .focus doesn't like css animations, set timeout to make sure input gets focus */
-			setTimeout(function() {
-				$('.navbar-bootsnipp .bootsnipp-search .form-control').focus();
-			}, 100);
-		}
-	});
-
-	$(document).on('keyup', function(event) {
-		if (event.which == 27 && $('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
-			$('a[href="#toggle-search"]').trigger('click');
-		}
-	});
+    });
 });
