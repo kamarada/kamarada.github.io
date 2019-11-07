cf4a4891752f5449d323ee2fcd61a516e5693fde
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2019-11-07 10:23:23 -0300

    Publishing website (see commit kamarada/kamarada-website@c2658ce11f090a99d08ca6a721a648cec655080f)

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,12 @@ function random_number(first, last) {
 
 $(document).ready(function(){
     $('body').bootstrapMaterialDesign();
+
+    $('#fab').click(function(){
+        var enterDesignMode = ($('#fab button i').text() == "visibility");
+        $('#fab button i').text(enterDesignMode ? "edit" : "visibility");
+        document.designMode = enterDesignMode ? "on" : "off";
+    });
 });
 
 // https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
